770a21dceed7407e5f71e1ef32abe311d8a433da
Shrinidhiimuthukumaran <mshr21202.it@rmkec.ac.in>
2025-04-12 18:27:58 +0530

    Shrinidhii's changes

--- src/com/hexaware/assetmanagementsystem/dao/AssetManagementDaoImp.java
+++ src/com/hexaware/assetmanagementsystem/dao/AssetManagementDaoImp.java
@@ -1,6 +1,38 @@
 package com.hexaware.assetmanagementsystem.dao;
 
+import com.hexaware.assetmanagementsystem.entity.Asset;
+
 public class AssetManagementDaoImp implements IAssetManagementDao{
+
+	@Override
+	public boolean addAsset(Asset asset) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateAsset(Asset asset) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteAsset(int assetId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean allocateAsset(int assetId, int employeeId, String allocationDate) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deallocateAsset(int assetId, int employeeId, String returnDate) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	@Override
 	public boolean performMaintenance(int assetId, String maintenanceDate, String description, double cost) {
--- src/com/hexaware/assetmanagementsystem/dao/IAssetManagementDao.java
+++ src/com/hexaware/assetmanagementsystem/dao/IAssetManagementDao.java
@@ -1,6 +1,8 @@
 package com.hexaware.assetmanagementsystem.dao;
 
 
+import com.hexaware.assetmanagementsystem.entity.Asset;
+
 /**
  *@Author: Rajalakshmi Ganesh
  *@Author: Shrinidhii Muthukumaran 
@@ -10,7 +12,51 @@ package com.hexaware.assetmanagementsystem.dao;
 public interface IAssetManagementDao {
 	
 	
+	/**
+	 * @author Shrinidhii Muthukumaran 
+	 * @param asset
+	 * @return
+	 */
+	  boolean addAsset(Asset asset);
+	  
+	  
+
+	  /**
+		 * @author Shrinidhii Muthukumaran 
+		 * @param Asset 
+		 * @return
+		 */
+	  boolean updateAsset(Asset asset);
+	  
+	  
+	  /**
+		 * @author Shrinidhii Muthukumaran 
+		 * @param assetId
+		 * @return
+		 */
+	  boolean deleteAsset(int assetId);
+	  
+	  
+	  /**
+		 * @author Shrinidhii Muthukumaran 
+		 * @param assetID
+		 * @param employeeId
+		 * @param allocationDate
+		 * @return
+		 */
+	  boolean allocateAsset(int assetId, int employeeId, String allocationDate);
+	  
+	  
+	  /**
+		 * @author Shrinidhii Muthukumaran 
+		 * @param assetID
+		 * @param employeeId
+		 * @param allocationDate
+		 * @return
+		 */
+	  boolean deallocateAsset(int assetId, int employeeId, String returnDate);
 	
+	  //---------------------------------------------------------------------
 	/**
 	 * @author Rajalakshmi Ganesh
 	 * @param assetId

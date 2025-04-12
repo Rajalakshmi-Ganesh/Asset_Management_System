09e7d467734868a24bf98d0b8b7e51a0d0368c60
Rajalakshmi-Ganesh <raja21213.it@rmkec.ac.in>
2025-04-12 18:57:31 +0530

    Rajalakshmi's Changes

--- src/com/hexaware/assetmanagementsystem/service/AssetManagementServiceImp.java
+++ src/com/hexaware/assetmanagementsystem/service/AssetManagementServiceImp.java
@@ -1,25 +1,28 @@
 package com.hexaware.assetmanagementsystem.service;
 
 import com.hexaware.assetmanagementsystem.dao.*;
+
 public class AssetManagementServiceImp implements IAssetManagementService {
 
+	AssetManagementDaoImp dao = new AssetManagementDaoImp();
+	
 	@Override
 	public boolean performMaintenance(int assetId, String maintenanceDate, String description, double cost) {
 		// TODO Auto-generated method stub
-		return false;
+		return dao.performMaintenance(assetId, maintenanceDate, description, cost);
 		
 	}
 
 	@Override
 	public boolean reserveAsset(int assetId, int employeeId, String reservationDate, String startDate, String endDate) {
 		// TODO Auto-generated method stub
-		return false;
+		return dao.reserveAsset(assetId, employeeId, reservationDate, startDate, endDate);
 	}
 
 	@Override
 	public boolean withdrawReservation(int reservationId) {
 		// TODO Auto-generated method stub
-		return false;
+		return dao.withdrawReservation(reservationId);
 	}
 
 }
